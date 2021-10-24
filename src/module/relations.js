85a0bd15e4b5e8d3f79498a2dbc026de03415b93
Abdullah Ghani <abdullah.ghani3@gmail.com>
2021-10-24 12:31:55 +0530

    subscrptions added

--- src/module/relations.js
+++ src/module/relations.js
@@ -1,27 +1,10 @@
-// const User = require("@app/module/user");
-// const Entity = require("@app/module/entity");
-// const Sop = require("@app/module/sopRequest");
+const User = require('@app/module/user')
+const Post = require('@app/module/post')
 
-// User.tc.addRelation("entity", {
-//   resolver: () => Entity.tc.getResolver("findById"),
-//   prepareArgs: {
-//     _id: (source) => source.entityId,
-//   },
-//   projection: { entity: 1 },
-// });
-
-// Sop.tc.addRelation("entity", {
-//   resolver: () => Entity.tc.getResolver("findById"),
-//   prepareArgs: {
-//     _id: (source) => source.entity,
-//   },
-//   projection: { entity: 1 },
-// });
-
-// Sop.tc.addRelation("createdBy", {
-//   resolver: () => User.tc.getResolver("findById"),
-//   prepareArgs: {
-//     _id: (source) => source.createdBy,
-//   },
-//   projection: { createdBy: 1 },
-// });
+Post.tc.addRelation('createdByUser', {
+  resolver: () => User.tc.getResolver('findById'),
+  prepareArgs: {
+    _id: source => source.createdBy
+  },
+  projection: { createdByUser: 1 }
+})
